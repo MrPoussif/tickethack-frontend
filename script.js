b8082a67fb50ebac897c248b01d8e35815f52e22
matchls <mathieu.chales@gmail.com>
2026-01-28 16:05:12 +0100

    ajout fonction totalPrice

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 console.log("script chargé");
 
-// bouton search
-document.querySelector("#btn-search").addEventListener("click", () => {
-  const departure = document.querySelector("#departure").value.trim();
-  const arrival = document.querySelector("#arrival").value.trim();
-  const date = document.querySelector("#date").value;
+if (document.querySelector("#btn-search")) {
+  // bouton search
+  document.querySelector("#btn-search").addEventListener("click", () => {
+    const departure = document.querySelector("#departure").value.trim();
+    const arrival = document.querySelector("#arrival").value.trim();
+    const date = document.querySelector("#date").value;
 
-  fetch("http://localhost:3000/index/search", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ departure, arrival, date }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const contentRight = document.querySelector("#content-right");
+    fetch("http://localhost:3000/index/search", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ departure, arrival, date }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const contentRight = document.querySelector("#content-right");
 
-      if (!data.result) {
-        contentRight.innerHTML = `<p class="state-text">Error</p>`;
-        return;
-      }
+        if (!data.result) {
+          contentRight.innerHTML = `<p class="state-text">Error</p>`;
+          return;
+        }
 
-      if (data.trips.length === 0) {
-        contentRight.innerHTML = `
+        if (data.trips.length === 0) {
+          contentRight.innerHTML = `
           <img class="state-empty-illustration" src="images/notfound.png" alt="">
           <p class="state-text">No trip found.</p>
         `;
-        return;
-      }
+          return;
+        }
 
-      contentRight.innerHTML = `
+        contentRight.innerHTML = `
         ${data.trips
           .map(
             (trip) => `
@@ -52,30 +53,49 @@ document.querySelector("#btn-search").addEventListener("click", () => {
           .join("")}
       `;
 
-      resetEventListener();
-    })
-    .catch(() => {
-      document.querySelector("#content-right").innerHTML =
-        `<p class="state-text">Server error</p>`;
-    });
-});
-
-function resetEventListener() {
-  document.querySelectorAll(".btn-book").forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const tripId = btn.dataset.id;
-
-      fetch("http://localhost:3000/index", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ tripId }),
+        resetEventListener();
       })
-        .then((res) => res.json())
-        .then((data) => {
-          if (!data.result) return console.error(data.error);
-          window.location.href = "cart.html";
-        })
-        .catch((err) => console.error("Erreur fetch add cart", err));
-    });
+      .catch(() => {
+        document.querySelector("#content-right").innerHTML =
+          `<p class="state-text">Server error</p>`;
+      });
   });
+
+  function resetEventListener() {
+    document.querySelectorAll(".btn-book").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const tripId = btn.dataset.id;
+
+        fetch("http://localhost:3000/index", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ tripId }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (!data.result) return console.error(data.error);
+            window.location.href = "cart.html";
+          })
+          .catch((err) => console.error("Erreur fetch add cart", err));
+      });
+    });
+  }
+}
+
+list = [
+  { departure: "Paris", arrival: "Marseille", price: "125€" },
+  { departure: "Paris", arrival: "Marseille", price: "98€" },
+  { departure: "Paris", arrival: "Marseille", price: "108€" },
+  { departure: "Paris", arrival: "Marseille", price: "57€" },
+  { departure: "Paris", arrival: "Marseille", price: "37€" },
+];
+
+function totalPrice(tripList) {
+  // ! Prends un tableau d'objets en entrée
+  total = 0;
+  for (let trip of tripList) {
+    console.log("price", trip.price);
+    total += parseInt(trip.price);
+  }
+  return total;
 }
